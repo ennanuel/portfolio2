63ea98bab107715fbf3ce54dbdb6512a8ae68d0b
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-30 23:30:44 +0100

    Redesigned the skills components

--- frontend/src/components/sections/Skills.jsx
+++ frontend/src/components/sections/Skills.jsx
@@ -2,7 +2,7 @@ import { Title, BgText } from '../decorations';
 import { useRef } from "react";
 import { useIsVisible } from 'react-is-visible';
 import { SKILLS } from '../../assets/data';
-import '../../styles/skills/skills.css';
+import '../../styles/skills.scss';
 
 const Skills = () => {
   const nodeRef = useRef();
@@ -10,7 +10,7 @@ const Skills = () => {
 
   return (
     <section ref={nodeRef} id="skills" title="02. Skills" className="section skills flex-center">
-      <Title number="02.">Skills</Title>
+      <Title>Skills</Title>
       <div className={`section-content ${isVisible ? 'animate__animated animate__fadeIn' : 'hide-element'}`} style={{ flexDirection: 'column', height: '100%' }}>
         <div className="skill-container">
           {
@@ -18,7 +18,7 @@ const Skills = () => {
               <article
                 key={title}
                 style={{ animationDelay: `${(i+3)/10}s`}}
-                className={`skill link full-border ${isVisible && 'animate__animated animate__fadeInUp'}`}
+                className={`skill link ${isVisible && 'animate__animated animate__fadeInUp'}`}
               >
                 <h2>{title}</h2>
                 <div className="line" />
@@ -26,8 +26,10 @@ const Skills = () => {
                   {
                     skills.map(({ name, Icon }) => (
                       <li key={name} className='poppins' link={name}>
-                        <Icon />
-                        <span>{name}</span>
+                        <div className="skill-detail full-border full-hw flex-row flex-center">
+                          <Icon size={60} />
+                          <span>{name}</span>
+                        </div>
                       </li>
                     ))
                   }
