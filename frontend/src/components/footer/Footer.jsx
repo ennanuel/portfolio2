2c53d0e7aa765ffd6b8192decdafd5b592f27e4c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-30 23:26:41 +0100

    Made a few design changes

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
-import { Highlight } from "../decorations";
+import { useContext } from "react";
 import Socials from "./Socials";
+import { PageContext } from "../../PageContext";
+import darkLogo from '../../assets/images/icon-dark.png';
+import lightLogo from '../../assets/images/icon-light.png';
 
 const Footer = () => {
+  const { theme, menu: { hide } } = useContext(PageContext);
   return (
-    <footer className="footer flex-center">
+    <footer className={`footer flex-col flex-center ${!hide && 'hide_footer'}`}>
+      <div className="footer-logo flex-center full-border">
+        <img src={theme === 'light' ? darkLogo : lightLogo} alt="Site Logo" />
+      </div>
       <Socials />
       <p>
-        Built and Designed by <Highlight>Emmanuel Ezema</Highlight>.
+        Built and Designed by <a href="https://www.github.com/ennanuel" target="_blank" className="poppins">Emmanuel Ezema</a>.
       </p>
     </footer>
   )
