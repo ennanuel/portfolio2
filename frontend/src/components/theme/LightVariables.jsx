b81716e831ac45e813939c2aaf7cc41b6cb1d55c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-30 23:31:10 +0100

    Made minor changes

--- frontend/src/components/theme/LightVariables.jsx
+++ frontend/src/components/theme/LightVariables.jsx
@@ -1,33 +1,34 @@
 const LightVariables = () => {
   return (
     <style>
-      {`:root {
-    --section-container-bg: rgb(255, 255, 255, 0.4);
+      {
+        `:root {
+    --section-container-bg: rgba(255, 255, 255, 0.4);
     --border: rgb(52, 86, 116);
     --text: rgb(35, 63, 90);
     --title-before-bg: rgb(52, 86, 116);
-    --highlight-text: rgb(106, 78, 98);
+    --highlight-text: rgb(30, 49, 65);
     --header2-color: rgb(35, 63, 90);
     --header2-secondary-color: rgb(75, 103, 130);
-    --menu-before-bg: rgba(213, 187, 197, 0.7);
-    --linear-color1: rgba(76, 56, 88, 0.4);
+    --menu-before-bg: rgba(200, 200, 200, 0.7);
+    --linear-color1: rgba(35, 63, 90, 0.4);
     --linear-color2: transparent;
-    --light-background: rgba(255, 255, 255, 0.1);
+    --light-background: rgba(197, 197, 197, 0.1);
     --skill-bg: rgba(0, 0, 0, 0.1);
-    --profile-overlay: rgba(106, 78, 98, 0.3);
+    --profile-overlay: rgba(255, 255, 255, 0.3);
     --box-shadow: rgba(0, 0, 0, 0.4);
-    --project-img-color: rgba(106, 78, 98, 0.6);
-    --project-description-color: rgb(186, 138, 157);
-    --contact-form-bg: rgb(76, 56, 88);
+    --project-img-color: rgba(255, 255, 255, 0.6);
+    --project-description-color: rgb(255, 255, 255);
+    --contact-form-bg: rgb(255, 255, 255);
     --project-link-color: rgb(35, 63, 90);
     --project-title-color: rgb(189, 224, 254);
     --project-text-color: rgba(35, 63, 90);
-    --social-icon-bg: rgb(213, 187, 197);
+    --social-icon-bg: rgb(255, 255, 255);
     --social-icon-after: rgb(35, 63, 90);
-    --main-background: rgb(213, 187, 197);
+    --main-background: rgb(230, 230, 230);
     --menu-linear-gradient: rgba(35, 63, 90, 0.6);
     --menu-btn-bg: rgb(35, 63, 90);
-    --task-bar-bg: rgb(213, 187, 197);
+    --task-bar-bg: rgb(255, 255, 255);
     --black-theme: rgb(0, 0, 0);
     --white-theme: rgb(255, 255, 255);
     --menu-bg: rgba(0, 0, 0, 0.9);
@@ -35,11 +36,11 @@ const LightVariables = () => {
     --scrollbar-bg: rgb(20, 15, 0);
     --number-color: rgb(189, 224, 254);
     --secondary-border-color: white;
-    --background-tile-color: rgb(255, 235, 242);
+    --background-tile-color: rgb(200, 200, 200);
     --taskbar-btn-color: rgb(42, 157, 143);
-    --contact-form-bg: rgb(213, 187, 197);
     --fake-text-color: rgba(38, 70, 83, 0.1);
-}`}
+    }`
+      }
     </style>
   )
 }
